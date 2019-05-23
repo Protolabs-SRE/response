0910d183bd80775809e6ef8aa4a9c1145269f5bb
Ashley Poole <git@ashleypoole.co.uk>
2019-05-23 10:18:47 +0100

    Re-add status page runbook env rename

--- slack/keyword_handlers.py
+++ slack/keyword_handlers.py
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 
 @keyword_handler(['status page', 'statuspage'])
 def status_page_notification(comms_channel: CommsChannel, user: str, text: str, ts: str):
-    comms_channel.post_in_channel(f"ℹ️ You mentioned the Status Page - <{settings.STATUS_PAGE_RUNBOOK}|here's the playbook> on how to put it up.")
+    comms_channel.post_in_channel(f"ℹ️ You mentioned the Status Page - <{settings.STATUS_PAGE_RUNBOOK_URL}|here's the playbook> on how to put it up.")
 
 @keyword_handler(['run book', 'runbook', 'play book', 'playbook'])
 def status_page_notification(comms_channel: CommsChannel, user: str, text: str, ts: str):
